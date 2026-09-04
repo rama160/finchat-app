99a71938f4b8678114c5c4dfa1fe963cd97c6c63
rama160 <63859069+rama160@users.noreply.github.com>
2026-09-04 23:56:30 +0800

    Update widget test to check FinchatApp type

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
+
 import 'package:finchat_app/main.dart';
 
 void main() {
   testWidgets('FinchatApp dapat dibuat', (WidgetTester tester) async {
     await tester.pumpWidget(const FinchatApp());
-    expect(find.text('Finchat AI'), findsOneWidget);
+
+    expect(find.byType(FinchatApp), findsOneWidget);
   });
 }
